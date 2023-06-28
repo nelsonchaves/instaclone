a2ce1ddc071e7a43add5bc2f363493110cbf9507
Nelson Chaves <nelsonchavespro@gmail.com>
2023-06-28 13:41:34 -0700

    sdfgdsfg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_182936) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_203503) do
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "likes", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
@@ -41,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_182936) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end

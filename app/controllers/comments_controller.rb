244bfa56b2e8acaf3dd01d2e4d660dd013fdb504
Nelson Chaves <nelsonchavespro@gmail.com>
2023-06-28 19:38:25 -0700

    finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
-class CommentsController
+class CommentsController < ApplicationController
+
   def new
   end
-  
+
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = Comment.new(comment_params)
-    comment.post = post
+    comment.post = @post
     comment.user = current_user
 
     if comment.save
@@ -13,4 +14,10 @@ class CommentsController
       render :new, status: :unprocessable_entity
     end
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
